63ffde00316c894f1632cee80a1b0e2b8c9967b1
mrmhthegreat <90477458+mrmhthegreat@users.noreply.github.com>
2022-01-20 00:37:30 +0500

    Update aboust.dart

--- lib/aboust.dart
+++ lib/aboust.dart
@@ -1,6 +1,6 @@
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app_dep_link/nav.dart';
+import 'nav.dart';
 
 class Abouts extends StatefulWidget {
   const Abouts({Key? key}) : super(key: key);
